4ea0d4e511cbb2bd36348e4892636356d0276677
shinyfuwante <mr.philliptang@gmail.com>
2022-04-08 16:32:06 -0700

    Establish basic CSS styling of the calculator
    This commit further fleshes out the general appearance of the calculator.

--- script.js
+++ script.js
@@ -25,3 +25,17 @@ function divide (a,b) {
     return Math.round(((a/b) * 100))/100; //rounds to two decimal places, may revisit later.
 }
 
+function operate (a,b, operator) {
+    switch (operator) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "x":
+            return multiply(a,b);
+        case "÷":
+            return divide(a,b);
+        default:
+            break;
+    }
+}
