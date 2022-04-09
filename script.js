f1c724306696f0582d05d735dd9bcb8bc4c55007
shinyfuwante <mr.philliptang@gmail.com>
2022-04-08 18:54:39 -0700

    Decorate buttons, instill display limit for digits.
    Buttons, display, and calculator are all rounded. In addition,
    the calculator will alert when the display has reached it's maximum
    digits and return.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const display = document.querySelector('display');
+const mathButtons = document.querySelectorAll('math-buttons');
+const clearButtons = document.querySelectorAll('clear-buttons');
+
+
 /* The four mathematical operations
 * Add and multiply are fairly basic.
 * Subtract uses the first number to subtract from the second.
@@ -31,7 +36,7 @@ function operate (a,b, operator) {
             return add(a,b);
         case "-":
             return subtract(a,b);
-        case "x":
+        case "×":
             return multiply(a,b);
         case "÷":
             return divide(a,b);
@@ -39,3 +44,31 @@ function operate (a,b, operator) {
             break;
     }
 }
+
+function mathClick(e) {
+    console.log(e.target);
+    if (e.target.classList.contains('operator')) console.log('operator');
+    else if (e.target.classList.contains('digit')) numPress(e.target.innerText);
+    else if (e.target.classList.contains('equals')) console.log('equals');
+}
+
+function clearClick(e) {
+    console.log(e.target);
+}
+
+function numPress(digit) {
+    //when a button is pressed, it will append the corresponding digit to the text body.
+    if (display.innerText.length >= 9) {
+        alert('Maximum digits exceeded!');
+        return;
+    }
+
+    display.innerText += String(digit);
+}
+
+function idle() {
+    const mathListener = mathButtons.forEach(row => row.addEventListener('click', mathClick));
+    const clearListener = clearButtons.forEach(button => button.addEventListener('click', clearClick));
+}
+
+idle();
